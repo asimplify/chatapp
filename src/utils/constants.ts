ae82799c8757812110cf91c72e4bacfd7592fe25
Work <55981656+asimplify@users.noreply.github.com>
2025-05-13 19:35:35 +0500

    add base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://a15f-2407-aa80-314-8d0c-71f3-16da-96e8-6213.ngrok-free.app";
+export const BASE_URL = "http://69.30.85.109:22033";
 export const USER_TOKEN_CHAT = "userToken_chat";
 export const toastDuration = 3000;
 
